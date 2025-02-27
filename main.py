b81fe4579f5c9e25b0035bd70c3a15b704bb686d
Valentin <valentin.llaomoles@gmail.com>
2025-02-27 09:48:57 +0100

    change ip

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ print("Data from Database:- ", record)
 
 app = FastAPI()
 
-if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8000)
 
 @app.get("/")
 def read_root():
@@ -26,3 +24,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='127.0.0.1', port=8000)
