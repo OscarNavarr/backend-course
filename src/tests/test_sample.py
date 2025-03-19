35586f8068e0479d441cea4929aa1aae2252f84e
Alex-Imerir <alexandre.chabre@imerir.com>
2025-03-19 13:57:56 +0100

    tests json

--- src/tests/test_sample.py
+++ src/tests/test_sample.py
@@ -1,10 +1,40 @@
 import pytest
+from fastapi.testclient import TestClient
+from main import app
 
+client = TestClient(app)
 
-def f():
-    raise SystemExit(1)
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "text/html" in response.headers["content-type"]
 
+def test_read_consultations():
+    response = client.get("/consultations")
+    assert response.status_code == 200
+    assert "consultations" in response.json()
 
-def test_mytest():
-    with pytest.raises(SystemExit):
-        f()
+def test_read_enseignants():
+    response = client.get("/enseignants")
+    assert response.status_code == 200
+    assert "enseignants" in response.json()
+
+def test_read_cours():
+    response = client.get("/cours")
+    assert response.status_code == 200
+    assert "cours" in response.json()
+
+def test_read_promotions():
+    response = client.get("/promotions")
+    assert response.status_code == 200
+    assert "promotions" in response.json()
+
+def test_read_utilisateurs():
+    response = client.get("/utilisateurs")
+    assert response.status_code == 200
+    assert "utilisateurs" in response.json()
+
+def test_read_salles():
+    response = client.get("/salles")
+    assert response.status_code == 200
+    assert "salles" in response.json()
