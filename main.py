9ad272109effade6fd46f8f44c1ebf8bf65fee19
Valentin <valentin.llaomoles@gmail.com>
2025-03-19 15:06:56 +0100

    Flake8

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -6,15 +6,23 @@ from fastapi import Request
 import psycopg2
 import uvicorn
 
+
 # Fonction pour créer une connexion à la base de données
 def get_db_connection():
-    return psycopg2.connect(database="val_max_alex_oscar", user="postgres", password="Aucun66", host="localhost", port=5432)
+    return psycopg2.connect(
+        database="val_max_alex_oscar",
+        user="postgres",
+        password="Aucun66",
+        host="localhost",
+        port=5432)
+
 
 app = FastAPI()
 
 # Configuration des templates et fichiers statiques
 templates = Jinja2Templates(directory="src/main")
 app.mount("/static", StaticFiles(directory="src/main/static"), name="static")
+
 
 # Récupération dynamique des données
 def fetch_data(query):
@@ -25,6 +33,7 @@ def fetch_data(query):
     cursor.close()
     connection.close()
     return data
+
 
 # Création des routes
 @app.get("/", response_class=HTMLResponse)  # Cette route gèrera la page d'acceuil/index
@@ -39,29 +48,36 @@ async def read_root(request: Request):      # Request est spécifique à Jinja2
             "salles": fetch_data("SELECT * FROM public.salles"),
         }, "index.html")
 
+
 @app.get("/consultations")
 def read_consultations():
-    return {"consultations" : fetch_data("SELECT * FROM public.consultations")}
+    return {"consultations": fetch_data("SELECT * FROM public.consultations")}
+
 
 @app.get("/enseignants")
 def read_enseignants():
-    return {"enseignants" : fetch_data("SELECT * FROM public.enseignants")}
+    return {"enseignants": fetch_data("SELECT * FROM public.enseignants")}
+
 
 @app.get("/cours")
 def read_cours():
-    return {"cours" : fetch_data("SELECT * FROM public.cours")}
+    return {"cours": fetch_data("SELECT * FROM public.cours")}
+
 
 @app.get("/promotions")
 def read_promotions():
-    return {"promotions" : fetch_data("SELECT * FROM public.promotions")}
+    return {"promotions": fetch_data("SELECT * FROM public.promotions")}
+
 
 @app.get("/utilisateurs")
 def read_utilisateurs():
-    return {"utilisateurs" : fetch_data("SELECT * FROM public.utilisateurs")}
+    return {"utilisateurs": fetch_data("SELECT * FROM public.utilisateurs")}
+
 
 @app.get("/salles")
 def read_salles():
-    return {"salles" : fetch_data("SELECT * FROM public.salles")}
+    return {"salles": fetch_data("SELECT * FROM public.salles")}
+
 
 # Code éxecuté au lancement de l'application
 if __name__ == "__main__":
